cc4457fe4a1440b0685338346b0fe85185d5aa11
Malcolm Walker <malcolm@ipatch.ca>
2020-08-21 21:55:03 -0700

    determine if other snakes are contesting for food

--- snakebrain.py
+++ snakebrain.py
@@ -312,6 +312,7 @@ def get_smart_moves(possible_moves, body, board, my_snake):
         food_moves = {}
         closest_food = []
         greed_moves = []
+        avoid_moves = []
 
         for path in food_choices:
             if any(food in safe_coords[path] for food in board["food"]):
@@ -323,9 +324,19 @@ def get_smart_moves(possible_moves, body, board, my_snake):
                 if food_moves[path] <= closest_food_distance:
                     print(f"safe food towards {path} is {closest_food_distance} or less")
                     closest_food.append(path)
+                    # TODO: update this test to see if we're the closest to the food, not to compare distance from 
+                    # us to food vs. enemy
                     if head_distance.get(path) and food_moves[path] < head_distance[path]:
-                        print(f"being greedy, closest head is {head_distance[path]}")
-                        greed_moves.append(path)
+                        for food in board["food"]:
+                            distance_to_me = get_minimum_moves(food, [my_snake["head"]])
+                            if distance_to_me == food_moves[path] + 1:
+                                for snake in enemy_snakes:
+                                    if get_minimum_moves(food, [snake["head"]]) < distance_to_me and snake["length"] >= my_snake["length"]:
+                                        # Don't
+                                        print(f'Avoiding food towards {path} because of {snake["name"]}')
+                                        avoid_moves.append(path)
+                        if not (path in avoid_moves):
+                            greed_moves.append(path)
         elif board["food"]:
             for path in food_choices:
                 food_moves[path] = get_minimum_moves(get_next(body[0], path), board["food"])
